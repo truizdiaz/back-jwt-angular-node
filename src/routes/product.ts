445f31d0494a973c1e297bf15e4797c9b09ac3cc
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-09-23 18:19:57 -0300

    protegemos rutas

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { getProducts } from '../controllers/product';
+import validateToken from './validate-token';
 
 const router = Router();
 
-router.get('/', getProducts)
+router.get('/',validateToken, getProducts)
 
 export default router;
---
+++ src/routes/validate-token.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    const headerToken = req.headers['authorization']
+
+
+    if (headerToken != undefined && headerToken.startsWith('Bearer ')) {
+        // Tiene token
+        try {
+            const bearerToken = headerToken.slice(7);
+            jwt.verify(bearerToken, process.env.SECRET_KEY || 'pepito123');
+            next()
+        } catch (error) {
+            res.status(401).json({
+                msg: 'token no valido'
+            })
+        }
+
+    } else {
+        res.status(401).json({
+            msg: 'Acceso denegado'
+        })
+    }
+
+}
+
+export default validateToken;
